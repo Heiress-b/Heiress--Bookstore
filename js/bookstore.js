becb52fa88f9f91b79dfc990b49a44ae0bf0b9da
Uwazie Success <uwaziesuccess02@gmail.com>
2024-10-12 10:32:09 -0700

    updates

--- js/bookstore.js
+++ js/bookstore.js
@@ -1,21 +1,15 @@
 let book_menu = [
-    { image: 'book-1.jpg', title: 'One Nation', price: 10, author: 'Ben Carson'},
-    { image: 'book-2.jpg', title: 'Gifted Hands', price: 12, author: 'Ben Carson'},
-    { image: 'book-3.jpg', title: 'Half of a Yellow Sun', price: 8, author: 'Chimamanda Adichie'},
-    { image: 'book-4.jpg', title: 'Made in Asian America', price: 2, author: 'Erika Lee'},
-    { image: 'book-5.jpg', title: 'Candle in the Darkness', price: 5, author: 'lauren'},
-    { image: 'book-6.jpg', title: 'Unbroken', price: 10, author: 'laura HillenBrand'},
-    { image: 'book-7.jpg', title: 'AMerican Gods', price: 10, author: 'Neil Gaiman'},
-    { image: 'book-8.jpg', title: 'One Nation', price: 10, author: 'Ben Carson'},
+    { image: 'book-1.jpg', title: 'One Nation', price: 10, author: 'Ben Carson', availability: true, lease_price: '6'},
+    { image: 'book-2.jpg', title: 'Gifted Hands', price: 12, author: 'Ben Carson', availability: true, lease_price: '8'},
+    { image: 'book-3.jpg', title: 'Half of a Yellow Sun', price: 8, author: 'Chimamanda Adichie', availability: false, lease_price: '5'},
+    { image: 'book-4.jpg', title: 'Made in Asian America', price: 12, author: 'Erika Lee', availability: true, lease_price: '8'},
+    { image: 'book-5.jpg', title: 'Candle in the Darkness', price: 5, author: 'lauren', availability: true, lease_price: '2'},
+    { image: 'book-6.jpg', title: 'Unbroken', price: 10, author: 'laura HillenBrand', availability: false, lease_price: '6'},
+    { image: 'book-7.jpg', title: 'AMerican Gods', price: 10, author: 'Neil Gaiman', availability: true, lease_price: '6'},
+    { image: 'book-8.jpg', title: 'One Nation', price: 10, author: 'Ben Carson', availability: false, lease_price: '6'},
 ];
 
 let searchInput = document.getElementById('enter-book-name'); 
-
-let localCart = localStorage.getItem('mycart');
-// let cart = !localCart ? [] : JSON.parse(localCart);
-
-
-// cartNumber();
 
 function loadbook() {
     let bookItems = '';
@@ -25,7 +19,10 @@ function loadbook() {
             <h3>${m.title}</h3>
             <p>$${m.price}</p>
             <p>${m.author}</p>
-            <a href="#display-cart"><button class="add-btn" onclick= "addToCart(${index})">Add to Cart</button></a>
+            <div class="justify-content-between">
+                <a href="#display-cart"><button class="add-btn" onclick= "addToCart(${index})">Add to Cart</button></a>
+                <a href="#display-cart"><button class="add-btn" onclick= "borrowBook('0')">Borrow Book</button></a>
+            </div>
         </div>`
     });
     document.getElementById('menu-section').innerHTML = bookItems;
@@ -41,7 +38,6 @@ function searchbook() {
     }
 }
 
-
 function displayBook(book) {
     let menulist = book_menu.find((x) => x.title == book)
     if(menulist) {
@@ -50,7 +46,7 @@ function displayBook(book) {
             <h3>${menulist.title}</h3>
             <p>$${menulist.price}</p>
             <p>${menulist.author}</p>
-            <button class="add-btn" onclick="">Add to Cart</button>
+            <button class="add-btn" onclick="addToCart(1)">Add to Cart</button>
         </div>`
         document.getElementById('menu-section').innerHTML = bookitem
         searchInput.value = ''
@@ -60,7 +56,7 @@ function displayBook(book) {
     }
     
 }
-let cart = [];
+cart = [];
 function addToCart(menuIndex) {
     let menuItem = book_menu[menuIndex];
     let cartSearch = cart.find((cartItem) => cartItem.title == menuItem.title);
@@ -75,13 +71,13 @@ function addToCart(menuIndex) {
         });
         updateLocalstorage ()
         alert(`${menuItem.title} added to cart`);
-        document.getElementById('display-cart').innerHTML +=`<div id="display-cart" class="col-6 d-lg-block mt-4" style="margin-left: 1px;">        
+        document.getElementById('display-cart').innerHTML +=`<div id="display-cart" class="col-12 col-lg-6 d-lg-block mt-4" style="margin-left: 1px;">        
             <div class="row rounded-2" style="border: 1px dashed; margin-left: 5px; padding: 10px 5px;">
                 <div class="col-6">
                     <p style="font-weight: bold;">Pack 1</p>
                 </div>
                 <div class="col-6 text-end">
-                    <i onclick="removeFood()"class="bi bi-trash" style="color: red;"></i>
+                    <i onclick="removeBook('0')"class="bi bi-trash" style="color: red;"></i>
                 </div>
                 <div class="col-6">
                     Name: ${menuItem.title}
@@ -108,13 +104,13 @@ function addToCart(menuIndex) {
         cartSearch.total = cartSearch.quantity * menuItem.price;
         alert('your cart has been updated')
         let ordered_book = ""
-        ordered_book +=`<div id="display-cart" class="col-6 d-lg-block mt-4" style="margin-left: 1px;">
+        ordered_book +=`<div id="display-cart" class="col-12 col-lg-6 d-lg-block mt-4" style="margin-left: 1px;">
             <div class="row rounded-2" style="border: 1px dashed; margin-left: 5px; padding: 10px 5px;">
                 <div class="col-6">
                     <p style="font-weight: bold;">Your Book</p>
                 </div>
                 <div class="col-6 text-end">
-                    <i onclick="removeFood()"class="bi bi-trash" style="color: red;"></i>
+                    <i onclick="removeBook('0')"class="bi bi-trash" style="color: red;"></i>
                 </div>
                 <div class="col-6">
                     Dish: ${menuItem.title}
@@ -150,16 +146,17 @@ function updateLocalstorage () {
     localStorage.setItem('mycart', JSON.stringify(cart));
 }
 
-
-
-function removeFood(name) {
+function removeBook(name) {
     let item = cart[name]
     let index = cart.find(cartitem => cartitem.name == item.name);
     if(index) {
         let notification = confirm('are you sure you would like to remove this item?')
         if(notification == true) {
             cart.splice(index)
+            alert('your order has been removed')
         }
+        document.getElementById('display-cart').innerHTML = ''
+        updateLocalstorage()
     }
 }
 
@@ -169,6 +166,14 @@ function sumCartTotal() {
         totalCost += cartItem.total
     });
     document.getElementById('display-total').innerHTML = `<p style="font-size: 30px;"> Your Total Bill is: $${totalCost}</p>`;
+}
+
+function sumLeaseCartTotal() {
+    let leasetotalCost = 0;
+    lease_cart.forEach(cartItem => {
+        totalCost += cartItem.total
+    });
+    document.getElementById('display-lease-total').innerHTML = `<p style="font-size: 30px;"> Your Total Bill is: $${leasetotalCost}</p>`;
 }
 
 function listCartItems() {
@@ -197,6 +202,85 @@ function listCartItems() {
 
     document.getElementById('display-cart').innerHTML = cartLi;
 }
+
+
+// function addBook(title) {
+//     let newBook = {
+//         title,
+//         author,
+//         Availability: true
+//     }
+//     Book_Info.push(newBook)
+//     return Book_Info;
+// }
+// let title = 'Beyound Intelligence'
+// let author = 'Uwazie Success Chinwekele'
+// let newly_added = addBook(title,author)
+// alert('successfully added')
+
+let lease_cart = [];
+function borrowBook(mytitle){
+    let book_genre = mytitle
+    let list = book_menu[mytitle];
+    let display = book_menu.find((x) => x.title == book_genre);
+   if(!display){
+        let notification = confirm(`Hello, Are you sure you would love to borrow this book with charges included?`)
+        if(notification == true) {
+            let quantity = 1
+            document.getElementById('display-cart').innerHTML +=`<div id="display-cart" class="col-12 col-lg-6 d-lg-block mt-4" style="margin-left: 1px;">        
+            <div class="row rounded-2" style="border: 1px dashed; margin-left: 5px; padding: 10px 5px;">
+                <div class="col-6">
+                    <p style="font-weight: bold;">Preview your Order(s)</p>
+                </div>
+                <div class="col-6 text-end">
+                    <i onclick="removeBook('0')"class="bi bi-trash" style="color: red;"></i>
+                </div>
+                <div class="col-6">
+                    Name: ${list.title}
+                    <p>Price: ${list.lease_price}</p>
+                    <p>Quantity: ${quantity}</p>
+                    <p>Total: ${list.lease_price}</p>
+                </div>
+                <div class="col-6 text-end">
+                    <button class="rounded-pill border-0 px-2"> - 1 + </button>
+                </div>
+                <div class="col-12 text-center">
+                    <a href="payment.html" style="color: white;">
+                        <button class="rounded-pill border-0 px-2 w-100" style="background-color: #02C27F;">Place Order</button>
+                    </a>
+                </div>
+            </div>
+            <div id="display-lease-total"></div>
+        </div>`
+        cartNumber();
+        sumLeaseCartTotal()
+        }
+    }else{
+        alert(`hello, ${list.title} is currently not available, kindly check another book`)
+    }
+    
+}
+
+// Display available books:
+// Create a function listAvailableBooks that lists all books that are available to borrow (i.e., isAvailable: true).
+// --------------------------------------
+
+function listAvailableBooks(book){
+    let available_items = book_menu[book];
+    let available_books = book_menu.filter(book => book.availability == true)
+    if(available_books) {
+        let book_choice = `<div id="menu-item" class="col-12 col-md-4">
+            <img src="/img/${available_items.image}" alt="" height="300px">
+            <h3>${available_items.title}</h3>
+            <p>$${available_items.price}</p>
+            <p>${available_items.author}</p>
+            <button class="add-btn" onclick="">Add to Cart</button>
+        </div>`
+        document.getElementById('menu-section').innerHTML = book_choice
+    }
+}
+
+
 
 listCartItems()
 sumCartTotal()
